537ae58346bc45223ab4a980ba5cd5c5935b7c0b
Uiratan Cavalcante <uiratan@gmail.com>
2025-02-07 14:59:04 -0300

    feat: outra versao de validador usando org.hibernate.validator.internal.constraintvalidators.hv.br
    refs DMF01-57

--- src/main/java/com/dmf/loja/validation/documento/CPFCNPJ.java
+++ src/main/java/com/dmf/loja/validation/documento/CPFCNPJ.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = DocumentoValidator.class)
+@Constraint(validatedBy = DocumentoValidatorProprio.class)
 @Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CPFCNPJ {
