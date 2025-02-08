8516179610f877eb2314ccee72d3c376b57e3da6
Uiratan Cavalcante <uiratan@gmail.com>
2025-02-08 17:51:50 -0300

    refactor: dependencia de ExisteId
    closes DMF01-48

--- src/main/java/com/dmf/loja/compra/ItemPedidoRequest.java
+++ src/main/java/com/dmf/loja/compra/ItemPedidoRequest.java
@@ -1,13 +1,13 @@
 package com.dmf.loja.compra;
 
 import com.dmf.loja.livro.Livro;
-import com.dmf.loja.validation.annotations.existeid.ExisteId;
+import com.dmf.loja.validation.annotations.existeid.ExisteNoBanco;
 import jakarta.persistence.EntityManager;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record ItemPedidoRequest(
-        @ExisteId(fieldName = "id", domainClass = Livro.class)
+        @ExisteNoBanco(fieldName = "id", domainClass = Livro.class)
         @NotNull Long idLivro,
         @Positive Integer quantidade
 ) {
