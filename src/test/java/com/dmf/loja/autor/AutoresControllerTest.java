8099cf09d03f22a468986f50cf88891aeaf88c21
Uiratan Cavalcante <uiratan@gmail.com>
2025-03-06 15:30:55 -0300

    test: Testes de api FechamentoCompra
    refs DMF01-51

--- src/test/java/com/dmf/loja/autor/AutoresControllerTest.java
+++ src/test/java/com/dmf/loja/autor/AutoresControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,6 +33,7 @@ class AutoresControllerTest {
 
     @Property(tries = 10)
     @Label("fluxo de cadastro de novo autor")
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
     void teste(@ForAll @AlphaChars @StringLength(min = 1, max = 255) String nome,
                @ForAll("emails") String email,
                @ForAll @AlphaChars @StringLength(min = 1, max = 255) String descricao) throws Exception {
